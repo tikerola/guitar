cd9d480f933f8d6ff8b0f0dd91d80b3d2cd4382d
timo kerola <timo.kerola@pp2.inet.fi>
2020-07-15 23:09:09 +0300

    minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="navbar bg-dark navbar-dark d-flex flex-row align-items-end p-2">
-      <p className="text-muted w-100 text-center">Copyright © 2020 Unreal Funreel</p>
+    <div className="navbar bg-dark navbar-dark d-flex flex-row align-items-end">
+      <p className="text-muted w-50 text-center pt-2">Copyright © 2020 Unreal Funreel</p>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,27 +5,31 @@ export default function Home() {
     <div className="w-100">
       <div className="d-flex flex-row justify-content-center bg-dark w-100">
         <img src="./assets/images/key.png" alt="guitar" height="120" />
-        <img src="./assets/images/title.png" className="text-right mb-3 p-3 text-white" alt="title" />
+        <div className="d-flex flex-column">
+          <img src="./assets/images/title.png" className="text-right mb-3 p-3 text-white" alt="title" />
+          <div className="text-muted position-absolute animated bounce infinite" style={{ top: '150px', left: '500px', textDecoration: 'underline' }}>
+            Take your playing to the next level</div>
+        </div>
         <img src="./assets/images/key1.png" alt="guitar" height="120" />
       </div>
       <div className="d-flex flex-row justify-content-around container">
         <div className="m-5 w-25">
           <h3>About Us</h3>
           <p className="">This page is dedicated to help guitarists to learn more about their instrument:
-notes, fretboard, intervals, scales etc.</p>
+            notes, fretboard, intervals, scales etc.</p>
           <p>
             Browse through and see if there's something that meets your particular needs.
-</p>
+          </p>
         </div>
-        <div className="m-5 w-50 d-flex flex-row">
+        <div className="m-5 w-50 d-flex flex-row align-self-center">
           <img src="./assets/images/yoda.png" alt="happy customer" height="120" />
           <div>
-            <q>This page with it's explicit content has given me so much through the years...</q>
+            <q><i>This page with it's explicit content has given me so much through the years... That
+            came out funny, but it's the truth.</i></q>
             <div className="text-right">
               <i>David B. Hernandez</i>
             </div>
           </div>
-
         </div>
 
 
