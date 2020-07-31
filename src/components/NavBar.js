3ce2187be490556f07fe76eaba20208d8654bf26
timo kerola <timo.kerola@pp2.inet.fi>
2020-07-31 23:24:04 +0300

    jep

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,16 +8,16 @@ export default withRouter(function NavBar({ location }) {
       <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
         <ul className="navbar-nav">
           <li className={`nav-item ${location.pathname === '/guitar/' && 'active'}`}>
-            <Link className="nav-link" to="/">Home</Link>
+            <Link className="nav-link" to="/guitar/">Home</Link>
           </li>
           <li className={`nav-item ${location.pathname === '/guitar/Fretboard-Mastery' && 'active'}`}>
-            <Link className="nav-link" to="Fretboard-Mastery">Fretboard</Link>
+            <Link className="nav-link" to="/guitar/Fretboard-Mastery">Fretboard</Link>
           </li>
           <li className={`nav-item ${location.pathname === '/guitar/Scales-Mastery' && 'active'}`}>
-            <Link className="nav-link" to="Scales-Mastery">Scales</Link>
+            <Link className="nav-link" to="/guitar/Scales-Mastery">Scales</Link>
           </li>
           <li className={`nav-item ${location.pathname === '/guitar/Ear-Training' && 'active'}`}>
-            <Link className="nav-link" to="Ear-Training">Ear Training</Link>
+            <Link className="nav-link" to="/guitar/Ear-Training">Ear Training</Link>
           </li>
           {/* <li className={`nav-item ${location.pathname === '/Caged' && 'active'}`}>
             <Link className="nav-link" to="Caged">Caged</Link>
